5fbb366a784fd0acb58951a41eab08f07b6a0820
sunnyshah7 <86511229+sunnyshah7@users.noreply.github.com>
2025-06-20 14:51:46 +0530

    Create print_practice.py

---
+++ print_practice.py
@@ -0,0 +1,40 @@
+# taking multiple input
+# x,y =input("enter the values").split()
+# print("value of x" ,x)
+# print("value of y",y)
+
+#taking conditional input
+
+# age = int(input("Enter your age:"))
+
+# if(age == 18):
+#     print("valid")
+# elif(age > 18):
+#     print("Not Valid")
+# else:
+#     print("Need to decide")     
+
+
+#finding datatypes in python
+
+# a = "rode"
+# b = 11.2
+# c = 12
+# d = ("geek", "for" ,"geeks")
+# e = {"geek" :1, "for" : 2 ,"geeks" : 3}
+# f = ["geek", "for" ,"geeks"]
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+
+# output formating old method
+
+# print("Geek:%2d , portal:%3f" %(12,2.5555))
+# print("Geek: %2o,portal:%2E"%(4,3))
+
+#output formating new method
+print("Geek {0}".format(1))
