0cf6075a9f79db2f64cee7259ccd2379a665b229
sunnyshah7 <86511229+sunnyshah7@users.noreply.github.com>
2025-06-20 16:16:28 +0530

    Create Loop_practices.py

---
+++ Loop_practices.py
@@ -0,0 +1,51 @@
+# age = int(input("Enter the age"))
+
+# if age >=18 :
+#     print("Adult")
+# elif age <18:
+#     print("Underage")
+# else:
+#     print("NTD")
+
+#another approach
+
+# marks = 45
+
+# res = "pass" if marks >=40 else "fail"
+# print(f"Result: {res}")
+
+#nested loops
+
+# age = int(input("Enter the age."))
+# is_eligible =True
+
+# if age <=60:
+#     if is_eligible:
+#         print("This person will get 40% Discount !")
+#     else:
+#         print("This person will get 10% Discount!")    
+# else:
+#     print("Not Eligible")
+
+#Python While Loop:
+
+# count = 0
+
+# while count <=3:
+#     count +=1
+#     print(f"Hello:{count}")
+
+# For Loop in Python
+
+# n=7
+# for i in range(1,n):
+#     print(i)
+
+# for loop in list
+
+# fruits =["apple", "banana","mango"]
+
+# for i in fruits:
+#     print(i)
+
+
