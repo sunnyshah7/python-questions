33637eda3b2c098eaacc66f9a6ce3656537bf9b4
Sunny Shah <86511229+sunnyshah7@users.noreply.github.com>
2025-06-24 19:30:27 +0530

    Create Recursion_practice.py

---
+++ Recursion_practice.py
@@ -0,0 +1,36 @@
+# def name(N,count):
+#     if count == 0:
+#         return
+#     print(N)
+#     return name(N,count-1)
+
+# name("sunny",10)
+
+# Power of a Number using Recursion in Python.
+
+# def power(b,p):
+#     if p == 0:
+#         return 1
+#     return b*power(b,p-1)
+
+# print(power(2,3))
+
+
+# def separate_even_odd(arr):
+#     even = []
+#     odd = []
+
+#     for num in arr:
+#         if num % 2 == 0:
+#             even.append(num)
+#         else:
+#             odd.append(num)
+
+#     return even + odd 
+
+
+# arr = [1,2,3,4,5,6,7,8,9]
+# result = separate_even_odd(arr)
+# print("Separated array:", result)
+
+
